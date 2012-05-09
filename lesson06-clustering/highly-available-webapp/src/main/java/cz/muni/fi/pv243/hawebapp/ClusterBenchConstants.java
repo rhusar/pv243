e411f459ca34b92f09176591347fe26141d520e7
Radoslav Husar <radosoft@gmail.com>
2012-05-09 18:59:34 +0200

    nc

--- lesson06-clustering/highly-available-webapp/src/main/java/cz/muni/fi/pv243/hawebapp/ClusterBenchConstants.java
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.pv243.hawebapp;
-
-public interface ClusterBenchConstants {
-
-    public static final String READONLY = "readonly";
-    public static final String INVALIDATE = "invalidate";
-}
--- lesson06-clustering/highly-available-webapp/src/main/java/cz/muni/fi/pv243/hawebapp/SessionScopedCdiSerialBean.java
+++
@@ -1,9 +0,0 @@
-package cz.muni.fi.pv243.hawebapp;
-
-import java.io.Serializable;
-import javax.enterprise.context.SessionScoped;
-import cz.muni.fi.pv243.hawebapp.SerialBean;
-
-@SessionScoped
-public class SessionScopedCdiSerialBean extends SerialBean implements Serializable {
-}
